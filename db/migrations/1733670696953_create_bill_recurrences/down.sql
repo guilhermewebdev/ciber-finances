c27576a64772c7fc46f358af147e5b546718822c
Guilherme Isaías <guilherme.silva@ferreri.co>
2024-12-08 12:22:02 -0300

    bill recurrences tables were added to migrations

---
+++ db/migrations/1733670696953_create_bill_recurrences/down.sql
@@ -0,0 +1,4 @@
+DROP TABLE bill_weekly_recurrences;
+DROP TABLE bill_month_recurrences;
+DROP TABLE bill_week_day_month_recurrences;
+DROP TABLE bill_year_recurrences;
---
+++ db/migrations/1733670759734_create_bill_due_dates/down.sql
@@ -0,0 +1,4 @@
+DROP TABLE bill_weekly_due_dates;
+DROP TABLE bill_month_due_dates;
+DROP TABLE bill_week_day_month_due_dates;
+DROP TABLE bill_year_due_dates;
